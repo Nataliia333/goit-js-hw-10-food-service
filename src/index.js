1403bea548c8ec22e19bcfc5e6f31b83235f2605
Nataliia333 <n.o.prykhodko@gmail.com>
2020-12-25 19:00:30 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './styles.css';
+import menuItems from './templates/menu-items.hbs';
+import menu from './menu.json';
+
+const ItemsRef = document.querySelector('.js-menu');
+const markup = menuItems(menu);
+ItemsRef.insertAdjacentHTML('beforeend', markup);
