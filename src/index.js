18514970d29007844ef97a8a6356a88d0592269d
Nataliia333 <n.o.prykhodko@gmail.com>
2021-01-03 18:16:52 +0200

    summery requied

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import './styles.css';
 import menuItems from './templates/menu-items.hbs';
 import menu from './menu.json';
+console.log(menuItems);
+console.log(menu);
 
 const ItemsRef = document.querySelector('.js-menu');
 const markup = menuItems(menu);
+
 ItemsRef.insertAdjacentHTML('beforeend', markup);
